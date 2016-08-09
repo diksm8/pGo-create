26a92c9b795aa3da125ce782d00fe9616348fe55
cocklover69 <cocklover69@users.noreply.github.com>
2016-08-09 17:08:37 +0200

    New branch

--- pgocreate.py
+++ pgocreate.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from pgoapi import PGoApi
 from pgoapi.utilities import f2i
 from pgoapi import utilities as util
@@ -18,48 +16,49 @@ sys.setdefaultencoding('utf-8')
 @click.option('--size', default=10, type=click.IntRange(6, 16, clamp=True), help='Username size, range between 5 and 20.')
 @click.option('--password', default=None, help='Password for accounts')
 @click.argument('outfile', default='accounts.json', required=False)
+
 def main(accounts, size, password, outfile):
 	"""This is a script to create Pokémon Go (PTC) accounts and accept the Terms of Service. Made by two skids who can't code for shit."""
 	counter = 0
-	driver = webdriver.Chrome()
-	outfile = open(outfile, 'r+' if os.path.exists(outfile) else 'w+')
-
-	# Load existing accounts
-	if (outfile.read(1) == ""):
-		accounts_array = []
-	else:
-		outfile.seek(0)
-		accounts_array = json.load(outfile)
+	accountStore = pokeAccountStore(outfile)
 
 	while counter != accounts:
-		username = id_generator(size)
+		username = idGenerator(size)
 		anonbox = make_anonbox()
 		email = anonbox[0]
-		_password = password if password != None else id_generator(12, string.ascii_uppercase + string.ascii_lowercase + string.digits)
+		_password = password if password != None else idGenerator(12, string.ascii_uppercase + string.ascii_lowercase + string.digits)
+
+		retryCount = 0
+		d = False
+		while retryCount < 10:
+			d = makeClubAccount(username, _password, email)
+			if d:
+				click.echo('Account %s created' % username)
+				break
+			else:
+				click.echo('Error while creating account %s, retrying (retry count %d)' % (username, retryCount))
+				retryCount += 1
+				continue
+		if d == False:
+			click.echo('Account %s couldn\'t be created, skipping.' % username)
+			continue
+
+		acceptTOS(username, _password)
+		click.echo('Accepted Terms of Service for user {}'.format(username))
+		d['TOS accepted'] = True
+		d['Email accepted'] = acceptEmail(anonbox[1], username)
+
+		accountStore.accounts.append(d)
+		accountStore.save()
+		counter += 1
 		
-		make_account(username, _password, email, driver)
-		accept_tos(username, _password)
-		email_accepted = accept_email(anonbox[1], username)
-
-		d = {
-			'Username': username,
-			'Password': _password,
-			'Email': email,
-			'Date created': time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
-			'ToS accepted': True,
-			'Email accepted': email_accepted
-		}
-		accounts_array.append(d)
-		outfile.seek(0)
-		json.dump(accounts_array, outfile, indent=4)
-		counter+=1
 		click.echo('Account %s written to file. Completed %s accounts.' % (username, counter)) 
-	outfile.close()
+	accountStore.done()
 
-def id_generator(size, chars=string.ascii_uppercase + string.digits):
+def idGenerator(size, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 
-def accept_email(box, username):
+def acceptEmail(box, username):
 	counter = 0
 	while counter != 60:
 		emails = requests.get(box, verify=False).text
@@ -72,7 +71,7 @@ def accept_email(box, username):
 				return True if 'Thank you for signing up! Your account is now active.' in requests.get(line).text else False
 	return False
 
-def accept_tos(username, password):
+def acceptTOS(username, password):
 	api = PGoApi()
 	api.set_position(40.7127837, -74.005941, 0.0)
 	api.login('ptc', username, password)
@@ -80,25 +79,62 @@ def accept_tos(username, password):
 	req = api.create_request()
 	req.mark_tutorial_complete(tutorials_completed = 0, send_marketing_emails = False, send_push_notifications = False)
 	response = req.call()
-	click.echo('Accepted Terms of Service for user {}'.format(username))
 
-def make_account(username, password, email, driver):
-	driver.get("https://club.pokemon.com/us/pokemon-trainer-club/sign-up/")
-	#elem = driver.find_element_by_name("dob")
-	driver.implicitly_wait(3)
-	driver.execute_script("document.getElementById('id_dob').removeAttribute('readonly')")
-	driver.execute_script("document.getElementById('id_dob').value='1986-12-12'")
-	driver.find_element_by_class_name('continue-button').click()
-	driver.find_element_by_id('id_username').send_keys(username)
-	driver.find_element_by_id('id_password').send_keys(password)
-	driver.find_element_by_id('id_confirm_password').send_keys(password)
-	driver.find_element_by_id('id_email').send_keys(email)
-	driver.find_element_by_id('id_confirm_email').send_keys(email)
-	driver.find_element_by_id('id_screen_name').send_keys(username)
-	driver.find_element_by_id('id_terms').click()
-	driver.find_element_by_class_name('button-green').click()
-	driver.refresh()
-	click.echo('Account %s created' % username)
+def makeClubAccount(username, password, email, dob='1986-12-12', country='US'):
+	headers = {
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Accept-Encoding': 'gzip, deflate, sdch, br',
+		'Accept-Language': 'pl-PL,pl;q=0.8,en-US;q=0.6,en;q=0.4',
+		'Cache-Control': 'max-age=0',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+	}
+	signupUrl  = 'https://club.pokemon.com/us/pokemon-trainer-club/sign-up/'
+	parentSignupUrl = 'https://club.pokemon.com/us/pokemon-trainer-club/parents/sign-up'
+
+	session = requests.Session()
+	session.headers.update(headers)
+	
+	# Get cookies first.
+	req = session.get(signupUrl)
+	if u'Create Your Pokémon Trainer Club Account' not in req.text:
+		return False
+	csrfCookie = requests.utils.dict_from_cookiejar(session.cookies)['csrftoken']
+
+	req = session.post(signupUrl, data={
+		'csrfmiddlewaretoken': csrfCookie,
+		'dob': dob,
+		'undefined': 7,
+		'undefined': dob.split('-')[0],
+		'country': country,
+		'country': country,
+	}, headers = {'Referer': signupUrl})
+	
+	if u'Your username is the name you will use to sign in to your account. Only you will see this name.' not in req.text:
+		return False
+
+	req = session.post(parentSignupUrl, data={
+		'csrfmiddlewaretoken': csrfCookie,
+		'username': username,
+		'password': password,
+		'confirm_password': password,
+		'email': email,
+		'confirm_email': email,
+		'public_profile_opt_in': True,
+		'screen_name': username,
+		'terms': 'on',
+	}, headers = {'Referer': parentSignupUrl})
+
+	if u'Thank you for creating a Pokémon Trainer Club account.' not in req.text:
+		return False
+
+	return {
+			'Username': username,
+			'Password': password,
+			'Email': email,
+			'Date created': time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
+			'TOS accepted': False,
+			'Email accepted': False
+	}
 
 def make_anonbox():
 	anonbox = requests.get('https://anonbox.net/en/', verify=False)
@@ -106,6 +142,22 @@ def make_anonbox():
 	address = tree.get_element_by_id('content').find('dl')[1].text_content()
 	inbox = tree.get_element_by_id('content').find('dl')[3].text_content()
 	return([address,inbox])
+
+class pokeAccountStore:
+	def __init__(self, accountsFile):
+		self.accountsFile = open(accountsFile, 'r+' if os.path.exists(accountsFile) else 'w+')
+		if (self.accountsFile.read(1) == ""):
+			self.accounts = []
+		else:
+			self.accountsFile.seek(0)
+			self.accounts = json.load(self.accountsFile)
+	def save(self):
+		self.accountsFile.seek(0)
+		json.dump(self.accounts, self.accountsFile, indent=2)
+	def done(self):
+		self.accountsFile.seek(0)
+		json.dump(self.accounts, self.accountsFile, indent=2)
+		self.accountsFile.close
 
 if __name__ == '__main__':
 	main()
